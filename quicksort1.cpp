5a5f8d311f2e2927c9d9e8c4a736850e8750a40c
kumudh-ranasinghe <127022266+kumudh-ranasinghe@users.noreply.github.com>
2023-03-30 08:03:06 +0530

    Update quicksort1.cpp

--- quicksort1.cpp
+++ quicksort1.cpp
@@ -7,53 +7,43 @@
 
 using namespace std;
 
-// Function to swap two elements in an array
 void swap(int* a, int* b) {
     int t = *a;
     *a = *b;
     *b = t;
 }
 
-// Partition function to return the pivot index
 int partition(int arr[], int left, int right) {
-    int pivot = arr[right];  // choose the rightmost element as pivot
-    int i = left - 1;  // Initialize i to be one index before the first element
+    int pivot = arr[right];  
+    int i = left - 1;  
     for (int j = left; j <= right - 1; j++) {
-        // If current element is smaller than or equal to pivot
         if (arr[j] <= pivot) {
-            i++;  // increment index of smaller element
-            swap(arr[i], arr[j]);  // swap the elements at i and j
+            i++;  
+            swap(arr[i], arr[j]);  
         }
     }
-    swap(arr[i + 1], arr[right]);  // swap the pivot element with the element at i+1
-    return (i + 1);  // return the index of pivot
+    swap(arr[i + 1], arr[right]);  
+    return (i + 1);  
 }
 
-// Function to perform Quick Sort iteratively
 void quickSort(int arr[], int left, int right) {
-    stack<int> stack;  // create a stack to store indices of subarrays
-    stack.push(left);  // push the indices of the first subarray onto the stack
+    stack<int> stack;  
+    stack.push(left);   
     stack.push(right);
 
     while (!stack.empty()) {
-        // pop the top two indices from the stack to get the next subarray
         right = stack.top();
         stack.pop();
         left = stack.top();
         stack.pop();
 
-        // find the pivot index and partition the subarray
         int p = partition(arr, left, right);
 
-        // If there are elements on the left side of pivot,
-        // push the left subarray onto the stack
         if (p - 1 > left) {
             stack.push(left);
             stack.push(p - 1);
         }
 
-        // If there are elements on the right side of pivot,
-        // push the right subarray onto the stack
         if (p + 1 < right) {
             stack.push(p + 1);
             stack.push(right);
@@ -61,7 +51,6 @@ void quickSort(int arr[], int left, int right) {
     }
 }
 
-// Function to print an array
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
@@ -76,7 +65,6 @@ int main() {
 
     int arr[n];
 
-    // Generate random array
     srand(time(0));
     for (int i = 0; i < n; i++) {
          arr[i] = rand() % 10001 + 10000;
@@ -90,10 +78,11 @@ int main() {
 
     cpu_time_used = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 
-    cout << "Sorted array is: ";
+    /*cout << "Sorted array is: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
-    }
+    }*/
+    
     cout << "Time taken: " << cpu_time_used << " nanoseconds" << endl;
 
     return 0;
